785544931a2a628fe9310ea60aaceab342d22c2f
alakhpandya <alakhpandya@gmail.com>
2023-03-31 20:03:21 +0530

    mar30

---
+++ css/project3/js/main.js
@@ -0,0 +1,21 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    dots:false,
+    // autoplay: true,
+    autoplayTimeout: 1000,
+    autoplayHoverPause: true,
+    navText: ['<i class="fa-solid fa-arrow-left-long nav-arrows"></i>', '<i class="fa-solid fa-arrow-right-long nav-arrows"></i>'],
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        }
+        // 1000:{
+        //     items:5
+        // }
+    }
+})
